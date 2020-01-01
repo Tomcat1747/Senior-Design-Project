e75e605e9b56e7443fa332e0df3e93aa0f4a9500
Thomas Bischoff <47576008+Tomcat1747@users.noreply.github.com>
2020-01-01 13:21:18 -0500

    Frozen Embers Unit Stats

---
+++ Stats of Units/StatInformation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StatInformation : ScriptableObject
+{
+    public int LevelupMagic;
+    public int LevelupDefense;
+    public int LevelupAttack;
+    public int LevelupSpeed;
+    public int LevelupRes;
+
+    public abstract void LevelUp(Unit unit);//Define unique levelup procedure for each unit
+    
+}
+
