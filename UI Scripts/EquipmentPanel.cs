5c8960ed6067cbc645f065755b6347616a422d8d
Thomas Bischoff <47576008+Tomcat1747@users.noreply.github.com>
2020-01-01 13:21:55 -0500

    Frozen Embers UI Scripts

---
+++ UI Scripts/EquipmentPanel.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EquipmentPanel : MonoBehaviour
+{
+    // Drop Down for the Type of Item the Player Can Have
+    public TMP_Dropdown itemOptions;
+    // List of Weapons
+    public GameObject weapons;
+    // List of Potions
+    public GameObject potions;
+    // List of Other Items
+    public GameObject other;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the Current Value of the Drop Down Box
+        int currentValue = itemOptions.value;
+        // Check if the Current Value is for the Weapons List
+        if(currentValue == 0)
+        {
+            // Display the Weapons to the Player
+            displayWeapons();
+        }
+        // Check if the Current Value is for the Potions List
+        else if(currentValue == 1)
+        {
+            // Display the Potions to the Player
+            displayPotions();
+        }
+        // Check if the Current Value is for the Other Items List
+        else if(currentValue == 2)
+        {
+            // Display the Other Items to the Player
+            displayOther();
+        }
+    }
+
+    // Displays the List of Weapons
+    private void displayWeapons()
+    {
+        // Set the Weapons Game Object to be Active
+        weapons.SetActive(true);
+        // Set the Potions Game Object to Not be Active
+        potions.SetActive(false);
+        // Set the Potions Game Object to Not be Actve
+        other.SetActive(false);
+    }
+
+    // Displays the List of Potions
+    private void displayPotions()
+    {
+        // Set the Weapons Game Object to Not be Active
+        weapons.SetActive(false);
+        // Set the Potions Game Object to be Active
+        potions.SetActive(true);
+        // Set the Potions Game Object to Not be Actve
+        other.SetActive(false);
+    }
+
+    // Displays the List of Other Items
+    private void displayOther()
+    {
+        // Set the Weapons Game Object to Not be Active
+        weapons.SetActive(false);
+        // Set the Potions Game Object to Not be Active
+        potions.SetActive(false);
+        // Set the Potions Game Object to be Actve
+        other.SetActive(true);
+    }
+}
---
+++ UI Scripts/HideUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HideUI : MonoBehaviour
+{
+    // Is the UI Showing
+    private bool uiShowing = true;
+    // Character Information Panel
+    public GameObject characterPanel;
+    // Skills Panel
+    public GameObject skillsPanel;
+    // Equipment Panel
+    public GameObject equipmentPanel;
+    // Objective Panel
+    public GameObject objectivePanel;
+    // Terrain Panel
+    public GameObject terrainPanel;
+    // All the Display Backgrounds
+    public GameObject uiBackgrounds;
+
+    // Toggles the UI
+    public void toggleUI()
+    {
+        // Check if the UI is Currently Showing
+        if(uiShowing)
+        {
+            // Turn Off All the Backgrounds of the Panels
+            uiBackgrounds.SetActive(false);
+            // Turn Off the Character Information Panel
+            characterPanel.SetActive(false);
+            // Turn Off the Skills Panel
+            skillsPanel.SetActive(false);
+            // Turn Off the Equipment Panel
+            equipmentPanel.SetActive(false);
+            // Turn Off the Objective Panel
+            objectivePanel.SetActive(false);
+            // Turn Off the Terrain Panel
+            terrainPanel.SetActive(false);
+            // Mark the the UI is Not Showing
+            uiShowing = false;
+        }
+        // Otherwise
+        else
+        {
+            // Turn On All the Backgrounds of the Panels
+            uiBackgrounds.SetActive(true);
+            // Turn On the Character Information Panel
+            characterPanel.SetActive(true);
+            // Turn On the Skills Panel
+            skillsPanel.SetActive(true);
+            // Turn On the Equipment Panel
+            equipmentPanel.SetActive(true);
+            // Turn On the Objective Panel
+            objectivePanel.SetActive(true);
+            // Turn On the Terrain Panel
+            terrainPanel.SetActive(true);
+            // Mark the the UI is Showing
+            uiShowing = true;
+        }
+    }
+}
